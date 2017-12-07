72b9b67b0ec3e433353366e78439fcfadbe71157
jiuweigui <jiuweigui@users.noreply.github.com>
2017-12-07 22:36:03 +0200

    remove zsh and vim

--- zsh/alias.zsh
+++
@@ -1,9 +0,0 @@
-# Alias for SSH so that it "supports" includes
-alias generate-ssh-config='cat ~/.ssh/config.d/* > ~/.ssh/config'
-
-# Aliases for MARKS
-alias j='jump'
-alias ml='marks'
-
-# Alias for tmuxifier
-alias mux='tmuxifier'
